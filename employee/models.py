c23e5a8c256450bd3bb30d0822ad35154b64d5fa
quiceno <quicenolondonoj@gmail.com>
2023-03-25 10:48:07 -0500

    crud con css

---
+++ employee/models.py
@@ -0,0 +1,12 @@
+from django.db import models  
+class Employee(models.Model):  
+    eid = models.CharField(max_length=25)  
+    ename = models.CharField(max_length=250)  
+    eemail = models.CharField(max_length=250)  
+    econtact = models.CharField(max_length=250) 
+    edistri = models.CharField(max_length=250)
+
+    def __str__(self):
+        return "%s " %(self.ename) 
+    class Meta:  
+        db_table = "employee"  

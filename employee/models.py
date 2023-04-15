2e84ba28a1d9896121b0b60f9b07d09c362b9e92
juan jose <quicenolondonoj@gmail.com>
2023-04-15 17:05:57 -0500

    avance en tablas semifuncional

---
+++ employee/models.py
@@ -0,0 +1,12 @@
+from django.db import models  
+class Employee(models.Model):  
+    epaciente = models.CharField(max_length=250)  
+    efechahora = models.DateTimeField()  
+    emedico = models.CharField(max_length=250) 
+    edistri = models.CharField(max_length=250)
+    ecolor = models.CharField(max_length=250)
+
+    def __str__(self):
+        return "%s " %(self.epaciente) 
+    class Meta:  
+        db_table = "citas"  
